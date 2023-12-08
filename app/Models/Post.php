2883ae96d78f76ff5c37ffac30b1c43a40ffdda4
imac <imac@MacBook-Air-de-imac.local>
2023-12-07 22:43:24 -0600

    relaciondo los modelos

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments(): HasMany {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function imageable(): MorphOne{
+        return $this->morphOne(Image::class, 'imageable');
+    }
 }
